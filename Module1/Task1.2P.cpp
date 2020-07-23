aa1176317e7516578c1d7e811c1f75d5903c50c5
RK-5 <62543713+RK-5@users.noreply.github.com>
2020-07-23 14:01:14 +1000

    Create Task1.2P.cpp

---
+++ Module1/Task1.2P.cpp
@@ -0,0 +1,24 @@
+const int G_LED = 3;
+const int PIR_Sensor = 2;
+int state = LOW;
+
+void setup()
+{
+  pinMode(PIR_Sensor, INPUT);  // Set Digital Pin 2 as Input (PIR Sensor)
+  pinMode(G_LED, OUTPUT); // Set Digital Pin 3 as Output (LED)
+  attachInterrupt(digitalPinToInterrupt(PIR_Sensor),motionDetected,RISING);
+  Serial.begin(9600); // Open the serial port at 9600 bps:
+}
+
+void motionDetected()
+{
+  state = digitalRead(PIR_Sensor);
+  digitalWrite(G_LED,state);
+  Serial.println("MOTION DETECTED");
+  
+
+}
+
+void loop()
+{
+}
