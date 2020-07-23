a32a481df43b7b3785eacd1131a81f96fe9e6396
RK-5 <62543713+RK-5@users.noreply.github.com>
2020-07-23 11:07:48 +1000

    Add files via upload

---
+++ SIT315_M1_1P.cpp
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(2, INPUT);  // Set Digital Pin 2 as Input (IR Sensor)
+  pinMode(3, OUTPUT); // Set Digital Pin 3 as Output (LED)
+  Serial.begin(9600); // Open the serial port at 9600 bps:
+}
+
+void loop()
+{
+  if (digitalRead(2) == HIGH) // If motion detected
+  {
+    Serial.println("MOTION DETECTED"); // Log Motion Detected
+    digitalWrite(3, HIGH);    // Turn LED ON
+  	delay(1000);              // Wait for 1000 millisecond(s)
+  	digitalWrite(3, LOW);     // Turn LED OFF
+  	delay(1000);              // Wait for 1000 millisecond(s)
+  }
+}
